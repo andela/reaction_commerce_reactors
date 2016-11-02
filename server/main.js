7d1bf1777ecc3f89dee8f04c760350f591f8004f
demiladebamgbose <demiladebamgbose94@gmail.com>
2016-11-02 17:59:56 +0100

    Feature: Setup express
    Set up express router
    
    Starts #133002651

--- server/main.js
+++ server/main.js
@@ -1,8 +1,10 @@
 import "./methods";
 import Startup from "./startup";
 import Security from "./security";
+import Api from "/api";
 
 Meteor.startup(() => {
+  Api();
   Startup();
   Security();
 });

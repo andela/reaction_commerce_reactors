c114b2c91aad30a00de260e7027d808778043610
Abisoye Oke-lawal <abisoye.oke-lawal@andela.com>
2016-11-07 13:27:19 +0100

    feature(sort-and-filter): create new branch for corrections - Add sort html view - Make filter and sort container fixed #133002629

--- imports/plugins/included/ui-search/client/templates/productSearch/SortnFilter.jsx
+++ imports/plugins/included/ui-search/client/templates/productSearch/SortnFilter.jsx
@@ -23,11 +23,24 @@ export default class SortnFilter extends TrackerReact(React.Component) {
 
   render() {
     return (
-      <div style={{ 'float': 'left', 'width': 200, 'margin': '0 20px', 'position': 'relative', 'zIndex': 2000 }}>
+      <div style={{ 'float': 'left', 'height': '350px', 'width': 200, 'margin': '0 20px', 'bottom': 0, 'position': 'fixed', 'zIndex': 2000, 'overflowY': 'scroll' }}>
+        <h5> Sort By:</h5>
+        <select name="sort">
+          <option value="lowToHigh"> Lowest - Highest Price</option>
+          <option value="lowToHigh"> Highest - Lowest Price</option>
+          <option value="lowToHigh"> Lowest - Highest Ratings</option>
+          <option value="lowToHigh"> Highest - Lowest Ratings</option>
+        </select>
+        <hr/>
         <h4>Filter</h4>
         <hr />
+        <h5>Brands</h5>
+        <div style={{'overflowY' : 'scroll', 'maxHeight' : '100px'}}>
+        {this.renderVendors()}
+        </div>
+        <hr />
         <div id="score" name="scoreName">
-          <h4>Best Sellers</h4>
+          <h5>Ratings</h5>
           <input value="4" name="bestSeller" type="radio" /> &#9733; &#9733; &#9733; &#9733; &#9733;<br />
           <input value="3" name="bestSeller" type="radio" /> &#9733; &#9733; &#9733; &#9733;<br />
           <input value="2" name="bestSeller" type="radio" /> &#9733; &#9733; &#9733;<br />
@@ -35,10 +48,7 @@ export default class SortnFilter extends TrackerReact(React.Component) {
           <input value="0" name="bestSeller" type="radio" /> &#9733; <br />
         </div>
         <hr />
-        <h6>Vendors</h6>
-        {this.renderVendors()}
-        <hr />
-        <h6>Price</h6>
+        <h5>Price</h5>
         <div id="min" style={{ 'display': 'inline-block' }}>
           min: <input type="text" />
         </div>

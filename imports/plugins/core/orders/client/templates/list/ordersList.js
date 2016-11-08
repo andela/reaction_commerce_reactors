f634474c22309534d722a9ab7b8dd12f50d21eae
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-08 12:46:57 +0100

    Feature(cancel-button-buyer): Add cancel button to customer order list

--- imports/plugins/core/orders/client/templates/list/ordersList.js
+++ imports/plugins/core/orders/client/templates/list/ordersList.js
@@ -2,16 +2,22 @@ import moment from "moment";
 import { Template } from "meteor/templating";
 import { Orders, Shops } from "/lib/collections";
 
+import FlatButton from "/imports/plugins/core/ui/client/components/button/flatButton"; // Import flatbutton react component
+
+
 /**
  * dashboardOrdersList helpers
  *
  */
 Template.dashboardOrdersList.helpers({
   orderStatus() {
-    // if (this.workflow.status === "coreOrderCompleted") {
-    //   return true;
-    // }
     return this.workflow.status.split("/")[1];
+  },
+  showCanceled() {
+    if (this.workflow.status === "coreOrderWorkflow/canceled" || this.workflow.status === "coreOrderWorkflow/completed") {
+      return false;
+    }
+    return true;
   },
   orders(data) {
     if (data.hash.data) {
@@ -33,5 +39,34 @@ Template.dashboardOrdersList.helpers({
   shopName() {
     const shop = Shops.findOne(this.shopId);
     return shop !== null ? shop.name : void 0;
+  },
+  // Create a helper to import in the FlatButton react component for
+  // cancelOrder button
+  CancelOrderButton() {
+    const order = this;
+    return  {
+      component: FlatButton,
+      kind: "flat",
+      className: "btn-danger btn-sm",
+      label: "Cancel order",
+      onClick() {
+        Alerts.alert({
+          title: "Cancel order",
+          text: `Are you sure you want to cancel this order,
+          your shipping charge would not be refunded if your
+          order has been shipped`,
+          showCancelButton: true,
+          confirmButtonText: "Cancel Order"
+        }, (isConfirm) => {
+          if (isConfirm) {
+            Meteor.call("orders/customerCancelOrder", order._id, function (error) {
+              if (error) {
+                console.log("error", error);
+              }
+            });
+          }
+        });
+      }
+    };
   }
 });

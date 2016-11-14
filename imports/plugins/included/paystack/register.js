f0367eb0ed9e265fbde76cbbeb1724ef0441b01c
bolorundurowb <ogatimo@gmail.com>
2016-11-14 10:57:16 +0100

    feature(paystack-option): adds the reaction register file for paystack

---
+++ imports/plugins/included/paystack/register.js
@@ -0,0 +1,42 @@
+/**
+ * Created by bolorundurowb on 11/14/16.
+ */
+/* eslint camelcase: 0 */
+import {Reaction} from "/server/api";
+
+Reaction.registerPackage({
+  label: "Paystack",
+  name: "paystack",
+  icon: "fa fa-credit-card-alt",
+  autoEnable: true,
+  settings: {
+    mode: false,
+    apiKey: ""
+  },
+  registry: [
+    // Dashboard card
+    {
+      provides: "dashboard",
+      label: "Paystack",
+      description: "Paystack payments",
+      icon: "fa fa-credit-card-alt",
+      priority: 3,
+      container: "paymentMethod"
+    },
+
+    // Settings panel
+    {
+      label: "Paystack Settings", // this key (minus spaces) is used for translations
+      route: "/dashboard/paystack",
+      provides: "settings",
+      container: "dashboard",
+      template: "paystackSettings"
+    },
+
+    // Payment form for checkout
+    {
+      template: "paystackForm",
+      provides: "paymentMethod"
+    }
+  ]
+});

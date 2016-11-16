1ddeab709b7ff85eec7b0d2e6ab28e95ee3b1f53
bolorundurowb <ogatimo@gmail.com>
2016-11-16 17:48:30 +0100

    feature(paystack-option): adds paystack popup when pay with paystack is clicked

--- imports/plugins/included/analytics/server/policy.js
+++ imports/plugins/included/analytics/server/policy.js
@@ -9,8 +9,5 @@ BrowserPolicy.content.allowOriginForAll("connect.facebook.net");
 BrowserPolicy.content.allowOriginForAll("fonts.googleapis.com");
 BrowserPolicy.content.allowOriginForAll("fonts.gstatic.com");
 BrowserPolicy.content.allowOriginForAll("js.paystack.co");
-<<<<<<< d7567ecf9d883a4409070eaaf9c105670a8f4738
 BrowserPolicy.content.allowOriginForAll("paystack.com");
 BrowserPolicy.content.allowOriginForAll("*.siftscience.com");
-=======
->>>>>>> feature(paystack-option): adds pay with paystack button and adjusts security policy to allow for loading the paystack script
--- server/security/policy.js
+++ server/security/policy.js
@@ -47,8 +47,5 @@ BrowserPolicy.content.allowOriginForAll("enginex.kadira.io");
 BrowserPolicy.content.allowOriginForAll("*.stripe.com");
 
 BrowserPolicy.content.allowOriginForAll("js.paystack.co");
-<<<<<<< d7567ecf9d883a4409070eaaf9c105670a8f4738
 BrowserPolicy.content.allowOriginForAll("paystack.com");
 BrowserPolicy.content.allowOriginForAll("*.siftscience.com");
-=======
->>>>>>> feature(paystack-option): adds pay with paystack button and adjusts security policy to allow for loading the paystack script
